a1cfe1f2e2524209334d2b27cd29eeface1d0fde
Defry <defryalex@gmail.com>
2022-05-01 01:05:07 +0500

    feat: make  styles and js code. In js make basic render and array with half of keys of keyboard

---
+++ pages/main/scripts/keys.js
@@ -0,0 +1,154 @@
+'use strict'
+
+export let KEYS = [
+	{
+		name_eng: '`',
+		name_ru: 'ё',
+		with_type: 'key-narrow1and5',
+		shift_eng: '~'
+	},
+	{
+		name_eng: '1',
+		name_ru: '1',
+		with_type: 'key-standart',
+		shift: '!',
+	},
+	{
+		name: '2',
+		with_type: 'key-standart',
+		shift_eng: '@',
+		shift_ru: '"'
+	},
+	{
+		name: '3',
+		with_type: 'key-standart',
+		shift_eng: '#',
+		shift_ru: '№'
+	},
+	{
+		name: '4',
+		with_type: 'key-standart',
+		shift_eng: '$',
+		shift_ru: ';'
+	},
+	{
+		name: '5',
+		with_type: 'key-standart',
+		shift: '%',
+	},
+	{
+		name: '6',
+		with_type: 'key-standart',
+		shift_eng: ':',
+		shift_ru: '^'
+	},
+	{
+		name: '7',
+		with_type: 'key-standart',
+		shift_eng: '?',
+		shift_ru: '&'
+	},
+	{
+		name: '8',
+		with_type: 'key-standart',
+		shift: '*',
+	},
+	{
+		name: '9',
+		with_type: 'key-standart',
+		shift: '(',
+	},
+	{
+		name: '0',
+		with_type: 'key-standart',
+		shift: ')',
+	},
+	{
+		name: '-',
+		with_type: 'key-standart',
+		shift: '_',
+	},
+	{
+		name: '=',
+		with_type: 'key-standart',
+		shift: '+',
+	},
+	{
+		name: 'Backspace',
+		with_type: 'key-wide2',
+	},
+	{
+		name: 'Tab',
+		with_type: 'key-wide1and5',
+	},
+	{
+		name_eng: 'q',
+		name_ru: 'й',
+		with_type: 'key-standart',
+	},
+	{
+		name_eng: 'w',
+		name_ru: 'ц',
+		with_type: 'key-standart',
+	},
+	{
+		name_eng: 'e',
+		name_ru: 'у',
+		with_type: 'key-standart',
+	},
+	{
+		name_eng: 'r',
+		name_ru: 'к',
+		with_type: 'key-standart',
+	},
+	{
+		name_eng: 't',
+		name_ru: 'е',
+		with_type: 'key-standart',
+	},
+	{
+		name_eng: 'y',
+		name_ru: 'н',
+		with_type: 'key-standart',
+	},
+	{
+		name_eng: 'u',
+		name_ru: 'г',
+		with_type: 'key-standart',
+	},
+	{
+		name_eng: 'i',
+		name_ru: 'ш',
+		with_type: 'key-standart',
+	},
+	{
+		name_eng: 'o',
+		name_ru: 'щ',
+		with_type: 'key-standart',
+	},
+	{
+		name_eng: 'p',
+		name_ru: 'з',
+		with_type: 'key-standart',
+		shift_eng: '?',
+		shift_ru: '&'
+	},
+	{
+		name_eng: '[',
+		name_ru: 'х',
+		with_type: 'key-standart',
+		shift_eng: '{',
+	},
+	{
+		name_eng: ']',
+		name_ru: 'ъ',
+		with_type: 'key-standart',
+		shift_eng: '}',
+	},
+	{
+		name: '\\',
+		with_type: 'key-standart',
+		shift_eng: '|',
+		shift_ru: '/'
+	},
+];
--- pages/main/scripts/main.js
+++ pages/main/scripts/main.js
@@ -1,1 +1,127 @@
-alert('Hello VK');
+'use strict'
+import { KEYS } from './keys.js';
+
+class Page {
+	constructor() {
+		this.header = null;
+		this.main = null;
+		this.renderPage();
+	}
+	renderPage() {
+		let body = document.querySelector('body');
+		this.header = new Header(body);
+		this.main = new Main(body);
+	}
+}
+
+class Header {
+	constructor(blockNode) {
+		this.renderHeader(blockNode);
+	}
+	renderHeader(blockNode) {
+		let headerHtml = `<header class="conteiner">
+							<h1 class='Head-page'>Virtual Keyboard</h1>
+							</header>`;
+		blockNode.insertAdjacentHTML('beforeend', headerHtml);
+	}
+}
+
+class Main {
+	constructor(blockNode) {
+		this.textArea = null;
+		this.keyboard = null;
+		this.ruEnSwitchInfo = null;
+		this.renderMain(blockNode);
+	}
+	renderMain(blockNode) {
+		let element = document.createElement("main")
+		element.classList.add('conteiner');
+		blockNode.append(element);
+		this.textArea = new TextArea(element);
+		this.keyboard = new Keyboard(element);
+		this.ruEnSwitchInfo = new RuEnSwitchInfo(element);
+	}
+}
+
+class TextArea {
+	constructor(blockNode) {
+		this.renderTextArea(blockNode);
+	}
+	renderTextArea(blockNode) {
+		let textAreaHtml = `<textarea class="display-area" name="display-text" id="display-text" cols="30" rows="10"></textarea>`;
+		blockNode.insertAdjacentHTML('beforeend', textAreaHtml);
+	}
+}
+
+class Keyboard {
+	constructor(blockNode) {
+		this.language = 'ENG';
+		this.keys = [];
+		this.blockNode = null;
+		this.renderKeyboard(blockNode, this.language);
+	}
+	renderKeyboard(blockNode, language) {
+		if (this.blockNode === null) {
+			let element = document.createElement("div")
+			element.classList.add('keyboard');
+			blockNode.append(element);
+			this.blockNode = element;
+		} else {
+			this.blockNode.innerHTML = '';
+			this.keys = [];
+		}
+
+		for (let i = 0; i < KEYS.length; i++) {
+			const key = new Key(KEYS[i], this.blockNode, language);
+			this.keys.push(key);
+		}
+		console.log(this.keys);
+	}
+}
+
+class Key {
+	constructor(key, blockNode, language) {
+		this.name_eng = key.name_eng;
+		this.name_ru = key.name_ru;
+		this.with_type = key.with_type;
+		this.shift_eng = key.shift_eng;
+		this.name = key.name;
+		this.with_type = key.with_type;
+		this.shift = key.shift;
+
+		this.renderKey(blockNode, language);
+	}
+	renderKey(blockNode, language) {
+		let sign = '';
+		if (language === 'ENG') {
+			sign = this.name_eng;
+		} else if (language === 'RU') {
+			sign = this.name_ru;
+		}
+
+		if (sign === undefined) {
+			sign = this.name;
+		}
+
+		let textAreaHtml = `<div class="key ${this.with_type}">${sign}</div>`;
+		blockNode.insertAdjacentHTML('beforeend', textAreaHtml);
+	}
+}
+
+class RuEnSwitchInfo {
+	constructor(blockNode) {
+		this.htmlText = null;
+		this.renderRuEnSwitchInfo(blockNode);
+	}
+	renderRuEnSwitchInfo(blockNode) {
+		let textAreaHtml = `<div class="ru-en-switch-info">
+								<p>Для переключения клавиатуры используйте сочетание клавиш ФФФФ + ПППП</p>
+							</div>`;
+		blockNode.insertAdjacentHTML('beforeend', textAreaHtml);
+		this.htmlText = blockNode.querySelector('.ru-en-switch-info');
+	}
+}
+
+const page = new Page();
+
+page.main.keyboard.renderKeyboard(page.main.keyboard.blockNode, 'RU');
