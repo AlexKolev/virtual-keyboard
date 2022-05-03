b20665981a4e6a0af21ef37c3eda191951c458b5
Defry <defryalex@gmail.com>
2022-05-03 19:56:38 +0500

    feat: make  change language, highlight buttons when button is pressed or key on keyboard is pressed

--- pages/main/scripts/keys.js
+++ pages/main/scripts/keys.js
@@ -5,150 +5,391 @@ export let KEYS = [
 		name_eng: '`',
 		name_ru: 'ё',
 		with_type: 'key-narrow1and5',
-		shift_eng: '~'
+		shift_eng: '~',
+		code: 'Backquote'
 	},
 	{
 		name_eng: '1',
 		name_ru: '1',
 		with_type: 'key-standart',
 		shift: '!',
+		code: 'Digit1',
 	},
 	{
 		name: '2',
 		with_type: 'key-standart',
 		shift_eng: '@',
-		shift_ru: '"'
+		shift_ru: '"',
+		code: 'Digit2'
 	},
 	{
 		name: '3',
 		with_type: 'key-standart',
 		shift_eng: '#',
-		shift_ru: '№'
+		shift_ru: '№',
+		code: 'Digit3'
 	},
 	{
 		name: '4',
 		with_type: 'key-standart',
 		shift_eng: '$',
-		shift_ru: ';'
+		shift_ru: ';',
+		code: 'Digit4'
 	},
 	{
 		name: '5',
 		with_type: 'key-standart',
 		shift: '%',
+		code: 'Digit5'
 	},
 	{
 		name: '6',
 		with_type: 'key-standart',
 		shift_eng: ':',
-		shift_ru: '^'
+		shift_ru: '^',
+		code: 'Digit6'
 	},
 	{
 		name: '7',
 		with_type: 'key-standart',
 		shift_eng: '?',
-		shift_ru: '&'
+		shift_ru: '&',
+		code: 'Digit7'
 	},
 	{
 		name: '8',
 		with_type: 'key-standart',
 		shift: '*',
+		code: 'Digit8'
 	},
 	{
 		name: '9',
 		with_type: 'key-standart',
 		shift: '(',
+		code: 'Digit9'
 	},
 	{
 		name: '0',
 		with_type: 'key-standart',
 		shift: ')',
+		code: 'Digit0'
 	},
 	{
 		name: '-',
 		with_type: 'key-standart',
 		shift: '_',
+		code: 'Minus'
 	},
 	{
 		name: '=',
 		with_type: 'key-standart',
 		shift: '+',
+		code: 'Equal'
 	},
 	{
 		name: 'Backspace',
 		with_type: 'key-wide2',
+		code: 'Backspace'
 	},
 	{
 		name: 'Tab',
 		with_type: 'key-wide1and5',
+		code: 'Tab'
 	},
 	{
 		name_eng: 'q',
 		name_ru: 'й',
 		with_type: 'key-standart',
+		code: 'KeyQ'
 	},
 	{
 		name_eng: 'w',
 		name_ru: 'ц',
 		with_type: 'key-standart',
+		code: 'KeyW'
 	},
 	{
 		name_eng: 'e',
 		name_ru: 'у',
 		with_type: 'key-standart',
+		code: 'KeyE'
 	},
 	{
 		name_eng: 'r',
 		name_ru: 'к',
 		with_type: 'key-standart',
+		code: 'KeyR'
 	},
 	{
 		name_eng: 't',
 		name_ru: 'е',
 		with_type: 'key-standart',
+		code: 'KeyT'
 	},
 	{
 		name_eng: 'y',
 		name_ru: 'н',
 		with_type: 'key-standart',
+		code: 'KeyY'
 	},
 	{
 		name_eng: 'u',
 		name_ru: 'г',
 		with_type: 'key-standart',
+		code: 'KeyU'
 	},
 	{
 		name_eng: 'i',
 		name_ru: 'ш',
 		with_type: 'key-standart',
+		code: 'KeyI'
 	},
 	{
 		name_eng: 'o',
 		name_ru: 'щ',
 		with_type: 'key-standart',
+		code: 'KeyO'
 	},
 	{
 		name_eng: 'p',
 		name_ru: 'з',
 		with_type: 'key-standart',
 		shift_eng: '?',
-		shift_ru: '&'
+		shift_ru: '&',
+		code: 'KeyP'
 	},
 	{
 		name_eng: '[',
 		name_ru: 'х',
 		with_type: 'key-standart',
 		shift_eng: '{',
+		code: 'BracketLeft'
 	},
 	{
 		name_eng: ']',
 		name_ru: 'ъ',
 		with_type: 'key-standart',
 		shift_eng: '}',
+		code: 'BracketRight'
 	},
 	{
 		name: '\\',
 		with_type: 'key-standart',
 		shift_eng: '|',
-		shift_ru: '/'
+		shift_ru: '/',
+		code: 'Backslash'
+	},
+	{
+		name: 'Caps Lock',
+		with_type: 'key-wide2',
+		code: 'CapsLock'
+	},
+	{
+		name_eng: 'a',
+		name_ru: 'ф',
+		with_type: 'key-standart',
+		code: 'KeyA'
+	},
+	{
+		name_eng: 's',
+		name_ru: 'ы',
+		with_type: 'key-standart',
+		code: 'KeyS'
+	},
+	{
+		name_eng: 'd',
+		name_ru: 'в',
+		with_type: 'key-standart',
+		code: 'KeyD'
+	},
+	{
+		name_eng: 'f',
+		name_ru: 'а',
+		with_type: 'key-standart',
+		code: 'KeyF'
+	},
+	{
+		name_eng: 'g',
+		name_ru: 'п',
+		with_type: 'key-standart',
+		code: 'KeyG'
+	},
+	{
+		name_eng: 'h',
+		name_ru: 'р',
+		with_type: 'key-standart',
+		code: 'KeyH'
+	},
+	{
+		name_eng: 'j',
+		name_ru: 'л',
+		with_type: 'key-standart',
+		code: 'KeyJ'
+	},
+	{
+		name_eng: 'k',
+		name_ru: 'л',
+		with_type: 'key-standart',
+		code: 'KeyK'
+	},
+	{
+		name_eng: 'l',
+		name_ru: 'д',
+		with_type: 'key-standart',
+		code: 'KeyL'
+	},
+	{
+		name_eng: ';',
+		name_ru: 'ж',
+		with_type: 'key-standart',
+		shift_eng: ':',
+		code: 'Semicolon'
+	},
+	{
+		name_eng: "'",
+		name_ru: 'э',
+		with_type: 'key-standart',
+		shift_eng: '"',
+		code: 'Quote'
+	},
+	{
+		name: 'Enter',
+		with_type: 'key-wide2',
+		code: 'Enter'
+	},
+	{
+		name: 'Shift',
+		with_type: 'key-wide2',
+		code: 'ShiftLeft'
+	},
+	{
+		name_eng: 'z',
+		name_ru: 'я',
+		with_type: 'key-standart',
+		code: 'KeyZ'
+	},
+	{
+		name_eng: 'x',
+		name_ru: 'ч',
+		with_type: 'key-standart',
+		code: 'KeyX'
+	},
+	{
+		name_eng: 'c',
+		name_ru: 'с',
+		with_type: 'key-standart',
+		code: 'KeyC'
+	},
+	{
+		name_eng: 'v',
+		name_ru: 'м',
+		with_type: 'key-standart',
+		code: 'KeyV'
+	},
+	{
+		name_eng: 'b',
+		name_ru: 'и',
+		with_type: 'key-standart',
+		code: 'KeyB'
+	},
+	{
+		name_eng: 'n',
+		name_ru: 'т',
+		with_type: 'key-standart',
+		code: 'KeyN'
+	},
+	{
+		name_eng: 'm',
+		name_ru: 'ь',
+		with_type: 'key-standart',
+		code: 'KeyM'
+	},
+	{
+		name_eng: ',',
+		name_ru: 'б',
+		with_type: 'key-standart',
+		shift_eng: '<',
+		code: 'Comma'
+	},
+	{
+		name_eng: '.',
+		name_ru: 'ю',
+		with_type: 'key-standart',
+		shift_eng: '>',
+		code: 'Period'
+
+	},
+	{
+		name_eng: '/',
+		name_ru: '.',
+		with_type: 'key-standart',
+		shift_eng: '?',
+		shift_ru: ',',
+		code: 'Slash'
+	},
+	{
+		name: '&uarr;',
+		with_type: 'key-standart',
+		code: 'ArrowUp'
+	},
+	{
+		name: 'Del',
+		with_type: 'key-wide2',
+		code: 'Delete'
+	},
+	{
+		name: 'Ctrl',
+		with_type: 'key-standart',
+		code: 'ControlLeft'
+	},
+	{
+		name: 'Win',
+		with_type: 'key-standart',
+		code: 'MetaLeft'
+	},
+	{
+		name_eng: 'ENG',
+		name_ru: 'RU',
+		with_type: 'key-standart',
+	},
+	{
+		name: 'Alt',
+		with_type: 'key-standart',
+		code: 'AltLeft'
+	},
+	{
+		name: 'Space',
+		with_type: 'key-wide5',
+		code: 'Space'
+	},
+	{
+		name: 'Alt',
+		with_type: 'key-standart',
+		code: 'AltRight'
+	},
+	{
+		name: 'Ctrl',
+		with_type: 'key-standart',
+		code: 'ControlRight'
+	},
+	{
+		name: '&larr;',
+		with_type: 'key-standart',
+		code: 'ArrowLeft'
+	},
+	{
+		name: '&darr;',
+		with_type: 'key-standart',
+		code: 'ArrowDown'
+	},
+	{
+		name: '&rarr;',
+		with_type: 'key-standart',
+		code: 'ArrowRight'
+	},
+	{
+		name: 'Shift',
+		with_type: 'key-standart',
+		code: 'ShiftRight'
 	},
 ];
--- pages/main/scripts/main.js
+++ pages/main/scripts/main.js
@@ -6,11 +6,62 @@ class Page {
 		this.header = null;
 		this.main = null;
 		this.renderPage();
+		this.keyDown = null;
+		this.notRender = null;
 	}
 	renderPage() {
 		let body = document.querySelector('body');
+
 		this.header = new Header(body);
 		this.main = new Main(body);
+		// listener для нажатия кнопоки
+		body.addEventListener('keydown', (e) => {
+			//console.log(e);
+			e.preventDefault();
+			let key = document.querySelector(`.key[data-code="${e.code}"]`);
+			//console.log(key);
+			if (key) {
+				this.main.keyboard.highLightButton(key);
+				if (e.code === 'ControlLeft') {
+					this.keyDown = 'ControlLeft';
+				} else if (this.keyDown === 'ControlLeft' && e.code === 'AltLeft') {
+					//переключение клавиатуры
+					this.main.keyboard.changeLanguage(document.body.main, this.main.keyboard.language, this.notRender);
+					this.notRender = 1;
+					// if (this.main.keyboard.language === 'ENG') {
+					// 	this.main.keyboard.renderKeyboard(document.body.main, 'RU');
+					// 	localStorage.setItem('language', 'RU');
+					// 	this.main.keyboard.language = 'RU';
+					// 	this.main.ruEnSwitchInfo.changeText('RU');
+					// } else if (this.main.keyboard.language === 'RU') {
+					// 	this.main.keyboard.renderKeyboard(document.body.main, 'ENG');
+					// 	localStorage.setItem('language', 'ENG');
+					// 	this.main.keyboard.language = 'ENG';
+					// 	this.main.ruEnSwitchInfo.changeText('ENG');
+					// }
+					//end переключение клавиатуры
+					//console.log(e.code);
+					this.main.keyboard.highLightButton(document.querySelector(`.key[data-code="${e.code}"]`));
+					this.main.keyboard.highLightButton(document.querySelector(`.key[data-code="${this.keyDown}"]`));
+				}
+			}
+		})
+		// listener для отжатия кнопоки
+		body.addEventListener('keyup', (e) => {
+			//	console.log(e);
+			let key = document.querySelector(`.key[data-code="${e.code}"]`);
+			if (key) {
+				this.main.keyboard.highLightButtonOff(key);
+				if (e.code === 'ControlLeft') {
+					this.keyDown = null;
+					this.notRender = null;
+				}
+				if (e.code === 'AltLeft') {
+					this.notRender = null;
+				}
+
+			}
+		})
 	}
 }
 
@@ -40,6 +91,7 @@ class Main {
 		this.textArea = new TextArea(element);
 		this.keyboard = new Keyboard(element);
 		this.ruEnSwitchInfo = new RuEnSwitchInfo(element);
+		this.keyboard.ruEnSwitchInfo = this.ruEnSwitchInfo;
 	}
 }
 
@@ -48,17 +100,53 @@ class TextArea {
 		this.renderTextArea(blockNode);
 	}
 	renderTextArea(blockNode) {
-		let textAreaHtml = `<textarea class="display-area" name="display-text" id="display-text" cols="30" rows="10"></textarea>`;
+		let textAreaHtml = `<textarea class="display-area" name="display-text" id="display-text" cols="30" rows="10" ></textarea>`;
 		blockNode.insertAdjacentHTML('beforeend', textAreaHtml);
 	}
 }
 
 class Keyboard {
-	constructor(blockNode) {
-		this.language = 'ENG';
+	constructor(blockNode, main) {
+		this.language = localStorage.getItem('language') ? localStorage.getItem('language') : 'ENG';
 		this.keys = [];
 		this.blockNode = null;
+		this.ruEnSwitchInfo = null;
 		this.renderKeyboard(blockNode, this.language);
+		blockNode.addEventListener('click', (e) => {
+			let target = e.target;
+			if (target.classList.contains('key')) {
+				this.changeLanguage(blockNode, target.innerText);
+				//console.log(target.innerText);
+				//переключение клавиатуры
+				// if (target.innerText === 'ENG') {
+				// 	this.renderKeyboard(blockNode, 'RU');
+				// 	localStorage.setItem('language', 'RU');
+				// 	this.language = 'RU';
+				// 	this.main.ruEnSwitchInfo.changeText('RU');
+				// } else if (target.innerText === 'RU') {
+				// 	this.renderKeyboard(blockNode, 'ENG');
+				// 	localStorage.setItem('language', 'ENG');
+				// 	this.language = 'ENG';
+				// 	this.main.ruEnSwitchInfo.changeText('RU');
+				// }
+				//end переключение клавиатуры
+			}
+		});
+		//listener on mousedown
+		blockNode.addEventListener('mousedown', (e) => {
+			let target = e.target;
+			this.highLightButton(target);
+		});
+		//выключение эфекта нажатия listener on mouseup
+		blockNode.addEventListener('mouseup', (e) => {
+			let target = e.target;
+			this.highLightButtonOff(target);
+		});
+		//выключение эфекта нажатия listener on mouseout, если нажали и переместили курср с кнопки
+		blockNode.addEventListener('mouseout', (e) => {
+			let target = e.target;
+			this.highLightButtonOff(target);
+		});
 	}
 	renderKeyboard(blockNode, language) {
 		if (this.blockNode === null) {
@@ -75,19 +163,54 @@ class Keyboard {
 			const key = new Key(KEYS[i], this.blockNode, language);
 			this.keys.push(key);
 		}
-		console.log(this.keys);
+	}
+	//включение эфекта нажатия
+	highLightButton(key) {
+		if (key.classList.contains('key')) {
+			//console.log('mousedown ' + target.innerText);
+			if (key.innerText === 'Caps Lock') {
+				key.classList.toggle('key-pres');;
+			} else {
+				key.classList.add('key-pres');
+			}
+		}
+	}
+	//выключение эфекта нажатия
+	highLightButtonOff(key) {
+		if (key.classList.contains('key')) {
+			if (key.innerText != 'Caps Lock') {
+				key.classList.remove('key-pres');
+			}
+		}
+	}
+	//переключение клавиатуры
+	changeLanguage(keyboard, language, render) {
+		if (!render) {
+			if (language === 'ENG') {
+				this.renderKeyboard(keyboard, 'RU');
+				localStorage.setItem('language', 'RU');
+				this.language = 'RU';
+				this.ruEnSwitchInfo.changeText('RU');
+			} else if (language === 'RU') {
+				this.renderKeyboard(keyboard, 'ENG');
+				localStorage.setItem('language', 'ENG');
+				this.language = 'ENG';
+				this.ruEnSwitchInfo.changeText('ENG');
+			}
+		}
 	}
 }
 
 class Key {
 	constructor(key, blockNode, language) {
+		this.name = key.name;
 		this.name_eng = key.name_eng;
 		this.name_ru = key.name_ru;
 		this.with_type = key.with_type;
-		this.shift_eng = key.shift_eng;
-		this.name = key.name;
-		this.with_type = key.with_type;
 		this.shift = key.shift;
+		this.shift_eng = key.shift_eng;
+		this.shift_ru = key.shift_ru;
+		this.code = key.code;
 
 		this.renderKey(blockNode, language);
 	}
@@ -102,8 +225,7 @@ class Key {
 		if (sign === undefined) {
 			sign = this.name;
 		}
-
-		let textAreaHtml = `<div class="key ${this.with_type}">${sign}</div>`;
+		let textAreaHtml = `<div class="key ${this.with_type}" data-code="${this.code}">${sign}</div>`;
 		blockNode.insertAdjacentHTML('beforeend', textAreaHtml);
 	}
 }
@@ -115,13 +237,25 @@ class RuEnSwitchInfo {
 	}
 	renderRuEnSwitchInfo(blockNode) {
 		let textAreaHtml = `<div class="ru-en-switch-info">
-								<p>Для переключения клавиатуры используйте сочетание клавиш ФФФФ + ПППП</p>
+								<p>Клавиатура создана в операционной системе Windiws</p>
+								<p>Для переключения клавиатуры используйте клавишу ENG на клавиатуре</p>
+								<p>Или комбинацию: левыe Crl + Alt</p>
 							</div>`;
 		blockNode.insertAdjacentHTML('beforeend', textAreaHtml);
 		this.htmlText = blockNode.querySelector('.ru-en-switch-info');
+	}
+	changeText(language) {
+		this.htmlText.innerHTML = '';
+		this.htmlText.innerHTML = `<p>Клавиатура создана в операционной системе Windiws</p>
+									<p>Для переключения клавиатуры используйте клавишу ${language} на клавиатуре</p>
+									<p>Или комбинацию: левыe Crl + Alt</p>`;
+
 	}
 }
 
 const page = new Page();
 
-page.main.keyboard.renderKeyboard(page.main.keyboard.blockNode, 'RU');
+// page.main.keyboard.renderKeyboard(page.main.keyboard.blockNode, 'RU');
+
+//localStorage.setItem('language', 'ENG');
+// let language = localStorage.getItem('language')
